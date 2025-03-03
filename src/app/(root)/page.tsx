180abd194b8e971523a3da2e6c354e52194fae98
andri <andriisumsel@gmail.com>
2025-03-03 13:38:15 +0700

    update token

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,15 @@
+import { cookies } from "next/headers";
 import ChatUserList from "./youapp/message/_components/chat-user-list";
+import { redirect } from "next/navigation";
 
 const page = () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    redirect("/login");
+  }
+
   return (
     <div className="h-screen w-full overflow-y-hidden">
       <ChatUserList />
